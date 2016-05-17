d1432bff046a2e106281b08d7816ebdf0c447c1e
Patrik Spathon <patrik@spathon.com>
2016-05-17 23:02:28 +0200

    Calculate correct answer

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const isWorking = require('../lib/isWorking');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Jobbar Janne?', answer: 'Ja' });
+  const answer = isWorking() ? 'Ja' : 'Nej';
+  res.render('index', { title: 'Jobbar Janne?', answer });
 });
 
 module.exports = router;
