4c3261250306beab883f5a9a8ee8cebaf269e34a
Patrik Spathon <patrik@spathon.com>
2016-05-17 23:00:59 +0200

    Quick test fix for cache

--- assets/sw.js
+++ assets/sw.js
@@ -4,6 +4,10 @@ console.info('Hello, I\'m your worker for ther day.');
 // Step 2 look in to
 // https://github.com/GoogleChrome/sw-precache
 
+// TODO: Fix cache and network fetch to update view correct,
+//       This should probably always fetch answer from server
+//       Or better calculate the answer offline
+
 const cacheName = 'jobbar-janne-0.1';
 const files = [
   '/',
@@ -42,17 +46,31 @@ self.addEventListener('fetch', (event) => {
   console.log('[SW] Fetch', event.request.url);
   event.respondWith(
     caches.match(event.request)
-      .then((response) => {
-        // Cache hit - return response
-        if (response) return response;
+      .then((cached) => {
+        /* Even if the response is in our cache, we go to the network as well.
+           This pattern is known for producing "eventually fresh" responses,
+           where we return cached responses immediately, and meanwhile pull
+           a network response and store that in the cache.
+           Read more:
+           https://ponyfoo.com/articles/progressive-networking-serviceworker
+        */
+        var networked = fetch(event.request)
+          // We handle the network request with success and failure scenarios.
+          .then(fetchedFromNetwork, unableToResolve)
+          // We should catch errors on the fetchedFromNetwork handler as well.
+          .catch(unableToResolve);
 
-        // IMPORTANT: Clone the request. A request is a stream and
-        // can only be consumed once. Since we are consuming this
-        // once by cache and once by the browser for fetch, we need
-        // to clone the response
-        const fetchRequest = event.request.clone();
+        /* We return the cached response immediately if there is one, and fall
+           back to waiting on the network as usual.
+        */
+        console.log('WORKER: fetch event', cached ? '(cached)' : '(network)', event.request.url);
+        return cached || networked;
 
-        return fetch(fetchRequest).then((response) => {
+        function fetchedFromNetwork(response) {
+          /* We copy the response before replying to the network request.
+             This is the response that will be stored on the ServiceWorker cache.
+          */
+
           // Check if we received a valid response
           if (
             !response ||
@@ -62,20 +80,58 @@ self.addEventListener('fetch', (event) => {
             return response;
           }
 
-          // IMPORTANT: Clone the response. A response is a stream
-          // and because we want the browser to consume the response
-          // as well as the cache consuming the response, we need
-          // to clone it so we have 2 stream.
-          var responseToCache = response.clone();
 
-          caches.open(cacheName)
-            .then((cache) => {
-              console.log('[SW] Fetched & cached the data');
-              cache.put(event.request, responseToCache);
+          var cacheCopy = response.clone();
+
+          console.log('WORKER: fetch response from network.', event.request.url);
+
+          caches
+            // We open a cache to store the response for this request.
+            .open(cacheName)
+            .then(function add(cache) {
+              /* We store the response for this request. It'll later become
+                 available to caches.match(event.request) calls, when looking
+                 for cached responses.
+              */
+              cache.put(event.request, cacheCopy);
+            })
+            .then(function() {
+              console.log('WORKER: fetch response stored in cache.', event.request.url);
             });
 
+          // Return the response so that the promise is settled in fulfillment.
           return response;
-        });
+        }
+
+        /* When this method is called, it means we were unable to produce a response
+           from either the cache or the network. This is our opportunity to produce
+           a meaningful response even when all else fails. It's the last chance, so
+           you probably want to display a "Service Unavailable" view or a generic
+           error response.
+        */
+        function unableToResolve () {
+          /* There's a couple of things we can do here.
+             - Test the Accept header and then return one of the `offlineFundamentals`
+               e.g: `return caches.match('/some/cached/image.png')`
+             - You should also consider the origin. It's easier to decide what
+               "unavailable" means for requests against your origins than for requests
+               against a third party, such as an ad provider
+             - Generate a Response programmaticaly, as shown below, and return that
+          */
+
+          console.log('WORKER: fetch request failed in both cache and network.');
+
+          /* Here we're creating a response programmatically. The first parameter is the
+             response body, and the second one defines the options for the response.
+          */
+          return new Response('<h1>Service Unavailable</h1>', {
+            status: 503,
+            statusText: 'Service Unavailable',
+            headers: new Headers({
+              'Content-Type': 'text/html'
+            })
+          });
+        }
       })
     );
 });
